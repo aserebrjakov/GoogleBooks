3b78bac395d73dc07aed5f55ea818605f3e8bef5
Alexandr Serebrjakov <tac.serebrjakov.a@gmail.com>
2019-06-21 18:08:59 +0300

    create new xcode project

---
+++ GoogleBooks/GoogleBooks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleBooks
+//
+//  Created by Alexandr on 21/06/2019.
+//  Copyright © 2019 Alexandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
